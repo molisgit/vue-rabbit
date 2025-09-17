b786581041dc25ff2d726c73bcbbe48e849e0b45
moli <2729614660@qq.com>
2025-09-17 16:26:17 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -9,3 +9,19 @@ export function getTopCategoryAPI(id) {
     },
   })
 }
+
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 二级分类id 
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => {
+  return request({
+    url: '/category/sub/filter',
+    params: {
+      id
+    }
+  })
+}
