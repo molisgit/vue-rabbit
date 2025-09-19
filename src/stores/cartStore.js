41737f0b7feb0a5bd2027ba2d1409db8720a6cc1
moli <2729614660@qq.com>
2025-09-19 19:22:19 +0800

    购物车删除功能bug修复；本地购物车-头部购物车-计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,10 +1,10 @@
-import { ref } from "vue"
+import { computed, ref } from "vue"
 import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', () => {
   // 1.定义statu
   const cartList = ref([])
-  // 2.定义action
+  // 2.1 定义action
   const addCart = (goods) => {
     // 添加购物车操作 已添加：count + 1 ，未添加： cartList.value.push 
     // 通过匹配传递过来的商品对象中的skuId,如果能在cartList中找到，就是添加过的商品
@@ -18,19 +18,34 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
-  // 定义删除购物车的action
+  // 2.2 定义删除购物车的action
   const delCart = (skuId) => {
     // 匹配cartList中的skuId,获得对应下标（索引）
-    const index = cartList.value.findIndex(item => {
-      skuId === item.skuId
-    })
-    cartList.value.splice(index,)
+    const index = cartList.value.findIndex(item => skuId === item.skuId)
+    cartList.value.splice(index, 1)
   }
+
+  // 3. 计算属性
+  // 3.1 总的数量 所有项的count之和
+  // 3.2 总的价格 所有项的count * price之和
+
+  const allCount = computed(() => {
+    return cartList.value.reduce((prev, cur) => {
+      return prev + cur.count
+    }, 0)
+  })
+  const allPrice = computed(() => {
+    return cartList.value.reduce((prev, cur) => {
+      return prev + cur.count * cur.price
+    }, 0)
+  })
 
   return {
     cartList,
+    allCount,
+    allPrice,
     addCart,
-    delCart
+    delCart,
   }
 }, {
   persist: true,
