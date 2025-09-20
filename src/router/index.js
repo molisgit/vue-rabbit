1349dbc91a59eaa8bd735a57dc0dd3acadabae60
moli <2729614660@qq.com>
2025-09-20 21:14:31 +0800

    路由配置以及基础数据的渲染

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCtaegory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import CheckOut from '@/views/CheckOut/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +35,10 @@ const router = createRouter({
         {
           path: '/cartlist',
           component: CartList,
+        },
+        {
+          path: '/checkout',
+          component: CheckOut,
         }
       ]
     },
