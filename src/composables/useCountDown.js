5ae1778be8de998da4b3d2f71139e06a77e90ca0
moli <2729614660@qq.com>
2025-09-21 19:43:48 +0800

    倒计时逻辑函数的实现

--- src/composables/useCountDown.js
+++ src/composables/useCountDown.js
@@ -1,27 +1,36 @@
-// 封装倒计时逻辑函数
-import { computed, onUnmounted, ref } from 'vue'
-import dayjs from 'dayjs'
+//封装倒计时逻辑函数
+
+import { computed, onUnmounted, ref } from "vue"
+import { dayjs } from "element-plus"
+
 export const useCountDown = () => {
-  // 1. 响应式的数据
   let timer = null
   const time = ref(0)
-  // 格式化时间 为 xx分xx秒
+  //格式化时间
   const formatTime = computed(() => dayjs.unix(time.value).format('mm分ss秒'))
-  // 2. 开启倒计时的函数
+  //开启倒计时的函数
   const start = (currentTime) => {
-    // 开始倒计时的逻辑
-    // 核心逻辑的编写：每隔1s就减一
+    //开启倒计时的逻辑
     time.value = currentTime
     timer = setInterval(() => {
+      //核心逻辑：每隔1s就减一
       time.value--
-    }, 1000)
+      //倒计时结束后自动清除定时器（避免内存泄漏）
+      if (time.value <= 0) {
+        clearInterval(timer)
+        timer = null //重置定时器变量
+      }
+    }, 1000);
   }
-  // 组件销毁时清除定时器
+
+  //组件销毁时清除定时器
   onUnmounted(() => {
     timer && clearInterval(timer)
   })
+
   return {
     formatTime,
     start
   }
+
 }
